d4892b410d7621afa5e8a7cc019a8348ba9e3e69
juliencassignol <julien.cassignol@balabit.com>
2018-02-09 18:38:49 +0100

    Added !bjcp

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ if(config.elasticsearch.enabled) {
 	});
 }
 
+var fs = require('fs'), xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+let bjcp = [];
+fs.readFile('./bjcp.xml', function(err, data) {
+	    parser.parseString(data, function (err, result) {
+	bjcp.push(result.styleguide.class[0]);
+    });
+});
 /**
  * Events
  */
@@ -147,6 +155,30 @@ bot.on('message', function(message) {
 				g=(Math.round(f*1000)/1000)
 				message.reply(g);
 			}
+		} else if(message.content.indexOf('!bjcp') == 0) {
+			var msg = message.content.substring ('!bjcp '.length).toUpperCase();
+			replied = false;
+			for(var i = 0; i < bjcp[0].category.length; i++) {
+				for(var j = 0; j < bjcp[0].category[i].subcategory.length; j++) {
+					var beer = bjcp[0].category[i].subcategory[j];
+					if(msg.localeCompare(beer.name[0].toUpperCase()) == 0) {
+						replied = true;
+						reply = beer.name+" ("+beer.$.id+") : IBU "+beer.stats[0].ibu[0].low+"-"+beer.stats[0].ibu[0].high;
+						reply += ", OG "+beer.stats[0].og[0].low+"-"+beer.stats[0].og[0].high;
+						reply += ", FG "+beer.stats[0].fg[0].low+"-"+beer.stats[0].fg[0].high;
+						reply += ", SRM "+beer.stats[0].srm[0].low+"-"+beer.stats[0].srm[0].high;
+						reply += ", Volts "+beer.stats[0].abv[0].low+"-"+beer.stats[0].abv[0].high;
+						reply += "\nExemples: "+beer.examples[0];
+						message.reply(reply);
+						break;
+					}
+				}
+				if(replied)
+					break;
+			}
+			if(!replied)
+				message.reply("Apatrouvé !");
+
 		} else if(message.content.indexOf('!help') == 0) {
 			message.author.createDM().then(function(channel) {
 				var message = 'Petite liste non exhaustive\n';
